5069ed0d6bd16d6fa22092b50461fc4b39462960
enzogilles <enzo.gilles@viacesi.fr>
2024-10-23 14:54:58 +0200

    Update main.ino

--- main.ino
+++ main.ino
@@ -38,6 +38,16 @@ unsigned long temps_appui_vert=0;
 
 int mesures[5];
 
+
+int LOG_INTERVAL = 10; // Valeur par défaut
+int FILE_MAX_SIZE = 4096; // Valeur par défaut
+int TIMEOUT = 30; // Valeur par défaut
+int LUMIN = 1; // Valeur par défaut
+int LUMIN_LOW = 200; // Valeur par défaut
+int LUMIN_HIGH = 700; // Valeur par défaut
+
+
+
 void Mesure_donnees(){
     for (int i=0;i<nb_capteurs;i++){
         mesures[i]=analogRead(capteurs[i]);
